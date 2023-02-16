bc85da48c58982a9e3ef592f3e5f0abd0df27bbd
Rene Ortega <reneortega2468@gmail.com>
2023-02-16 15:57:49 -0500

    Share page

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -1,58 +1,178 @@
 import React from "react";
-import { View, StyleSheet, Text, Share, FlatList, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Share,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  SafeAreaView,
+} from "react-native";
+
 import { get } from "../Api/get";
+
+import { Ionicons } from "@expo/vector-icons";
+
 const Home = () => {
-  const [data, setData] = React.useState([]);
+  // const [data, setData] = React.useState([]);
 
-  React.useEffect(() => {
-    // use get() here
-    get()
-      .then((data) => {
-        let { results } = data;
+  let data = [
+    {
+      id: 782585,
+      image: "https://spoonacular.com/recipeImages/782585-312x231.jpg",
+      imageType: "jpg",
+      title: "Cannellini Bean and Asparagus Salad with Mushrooms",
+    },
+    {
+      id: 716426,
+      image: "https://spoonacular.com/recipeImages/716426-312x231.jpg",
+      imageType: "jpg",
+      title: "Cauliflower, Brown Rice, and Vegetable Fried Rice",
+    },
+    {
+      id: 715497,
+      image: "https://spoonacular.com/recipeImages/715497-312x231.jpg",
+      imageType: "jpg",
+      title: "Berry Banana Breakfast Smoothie",
+    },
+    {
+      id: 715415,
+      image: "https://spoonacular.com/recipeImages/715415-312x231.jpg",
+      imageType: "jpg",
+      title: "Red Lentil Soup with Chicken and Turnips",
+    },
+    {
+      id: 716406,
+      image: "https://spoonacular.com/recipeImages/716406-312x231.jpg",
+      imageType: "jpg",
+      title: "Asparagus and Pea Soup: Real Convenience Food",
+    },
+    {
+      id: 644387,
+      image: "https://spoonacular.com/recipeImages/644387-312x231.jpg",
+      imageType: "jpg",
+      title: "Garlicky Kale",
+    },
+    {
+      id: 715446,
+      image: "https://spoonacular.com/recipeImages/715446-312x231.jpg",
+      imageType: "jpg",
+      title: "Slow Cooker Beef Stew",
+    },
+    {
+      id: 782601,
+      image: "https://spoonacular.com/recipeImages/782601-312x231.jpg",
+      imageType: "jpg",
+      title: "Red Kidney Bean Jambalaya",
+    },
+    {
+      id: 795751,
+      image: "https://spoonacular.com/recipeImages/795751-312x231.jpg",
+      imageType: "jpg",
+      title: "Chicken Fajita Stuffed Bell Pepper",
+    },
+    {
+      id: 766453,
+      image: "https://spoonacular.com/recipeImages/766453-312x231.jpg",
+      imageType: "jpg",
+      title: "Hummus and Za'atar",
+    },
+  ];
 
-        console.log(results);
-        setData(results);
-      })
-      .catch((error) => {
-        console.log(error);
+  // React.useEffect(() => {
+  //   // use get() here
+  //   get()
+  //     .then((data) => {
+  //       let { results } = data;
+
+  //       console.log(results);
+  //       setData(results);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }, []);
+
+  const shareFunc = async (id) => {
+    console.log("ID: ", id);
+    try {
+      const result = await Share.share({
+        message: "Check out this recipe!",
+        url: `exp://192.168.0.3:19000/--/Shared?id=${id}`,
       });
-  }, []);
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const [likedIds, setLikedIds] = React.useState([]);
+
+  const toggleLike = (id) => {
+    if (likedIds.includes(id)) {
+      // Remove the recipe id from the likedIds array
+      setLikedIds(likedIds.filter((likedId) => likedId !== id));
+    } else {
+      // Add the recipe id to the likedIds array
+      setLikedIds([...likedIds, id]);
+    }
+  };
+
+  const isLiked = (id) => likedIds.includes(id);
 
   const renderedItems = ({ item }) => {
-    return (
-      <View key={item.id} style={styles.data}>
-        <View style={styles.imageContainer}>
-          <Image
-            style={styles.image}
-            source={{
-              uri: item.image,
-            }}
-          />
+    const { id, image, title } = item;
 
-          <View style={styles.textContainer}>
-            <Text>{item.name}</Text>
-          </View>
+    return (
+      <View style={styles.containerBox}>
+        <View style={styles.back}>
+          <Image source={{ uri: image }} style={styles.image} />
+        </View>
+        <Text style={styles.text}>{title}</Text>
+        {/* add  like button and share */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 150,
+          }}
+        >
+          <TouchableOpacity onPress={() => toggleLike(id)}>
+            <Ionicons
+              name="heart"
+              size={24}
+              color={isLiked(id) ? "red" : "black"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => shareFunc(id)}>
+            <Ionicons name="share-social" size={24} color="black" />
+          </TouchableOpacity>
         </View>
       </View>
     );
   };
 
   return (
-    <View
-      style={{
-        height: "100%",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <FlatList
-        data={data}
-        renderItem={renderedItems}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={3}
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#2f4f4f" }}>
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          renderItem={renderedItems}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -62,30 +182,38 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontSize: 20,
-  },
-  data: {
-    flex: 1,
-    flexDirection: "column",
-    margin: 1,
-  },
-  imageContainer: {
-    flex: 1,
-    height: 100,
+  containerBox: {
+    width: 170,
+    height: 250,
+    margin: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    elevation: 5,
+    alignItems: "center",
+    justifyContent: "center",
   },
   image: {
-    width: 125,
-    height: 125,
-    margin: 5.5,
+    height: 100,
+    width: 100,
     borderRadius: 10,
-    paddingBottom: 20,
+    marginTop: 15,
   },
-  textContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 5,
+  back: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
 
--- Screens/Shared.jsx
+++ Screens/Shared.jsx
@@ -1,23 +1,57 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { useRoute } from "@react-navigation/native";
 
 const Shared = () => {
+  const route = useRoute();
+
+  const [loading, setLoading] = React.useState(true);
+
+  if (!route.params) return null;
+
+  const { id } = route.params;
+
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getReci() {
+      await fetch(
+        `https://api.spoonacular.com/recipes/${id}/summary?apiKey=1de7ce9afa06453da20342dd9eec2217`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const summary = data.summary.replace(/(<([^>]+)>)/gi, "");
+          setData(summary);
+          console.log("DTA: ", summary);
+        });
+    }
+    getReci();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Shared</Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View style={styles.container}>
+          <Text style={styles.text}>Shared ID: {id}</Text>
+          <Text style={styles.text}>Shared ID: {id}</Text>
+          <Text style={styles.text}>Shared ID: {id}</Text>
+        </View>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 20,
   },
-})
+});
 
 export default Shared;
